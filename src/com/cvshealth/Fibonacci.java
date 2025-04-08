a55671faad68645ed29b2da1734163cd4abb7d7c
vanand2cvs <vanand2cvs@gmail.com>
2025-04-08 00:39:19 -0500

    Revert to example 5

--- src/com/cvshealth/Fibonacci.java
+++ src/com/cvshealth/Fibonacci.java
@@ -4,16 +4,17 @@ public class Fibonacci {
 
 	public static void main(String[] args) {
 		
-   int i = 1, n = 10, firstTerm = 0, secondTerm = 1;
-    System.out.println("Fibonacci Series till " + n + " terms:");
+	    int i = 1, n = 10, firstTerm = 0, secondTerm = 1;
+	    System.out.println("Fibonacci Series till " + n + " terms:");
 
-   for (int i = 1; i <= n; ++i) {
-      System.out.print(firstTerm + ", ");
+	    while (i <= n) {
+	      System.out.print(firstTerm + ", ");
 
-      // compute the next term
-      int nextTerm = firstTerm + secondTerm;
-      firstTerm = secondTerm;
-      secondTerm = nextTerm;
+	      int nextTerm = firstTerm + secondTerm;
+	      firstTerm = secondTerm;
+	      secondTerm = nextTerm; 
+
+	      i++;
 	    }
 	  }
 	}
