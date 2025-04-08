0e3872642a7f9ecff6e6a918d8c52ea5b3db5307
vanand2cvs <vanand2cvs@gmail.com>
2025-04-08 00:34:20 -0500

    Change-Id: I0000000000000000000000000000000000000000 Signed-off-by: vanand2cvs <vanand2cvs@gmail.com>
    Change-Id: I6973c8a3251bbd40a2e91b4aeb8a46b4333d2444

--- src/com/cvshealth/Fibonacci.java
+++ src/com/cvshealth/Fibonacci.java
@@ -4,17 +4,16 @@ public class Fibonacci {
 
 	public static void main(String[] args) {
 		
-	    int i = 1, n = 10, firstTerm = 0, secondTerm = 1;
-	    System.out.println("Fibonacci Series till " + n + " terms:");
-
-	    while (i <= n) {
+		int n = 100, firstTerm = 0, secondTerm = 1;
+        
+	    System.out.println("Fibonacci Series Upto " + n + ": ");
+	    
+	    while (firstTerm <= n) {
 	      System.out.print(firstTerm + ", ");
 
 	      int nextTerm = firstTerm + secondTerm;
 	      firstTerm = secondTerm;
 	      secondTerm = nextTerm;
-
-	      i++;
 	    }
 	  }
 	}
